0ffd80556d8530993deeb314b1906a897eef05d6
IVB107 <ianvb107@gmail.com>
2019-02-06 22:47:31 -0600

    Adjusted layout styling

--- JS/index.js
+++ JS/index.js
@@ -20,10 +20,31 @@ learnMoreBtn.addEventListener('click', () => {
 class Card {
     constructor(card){
         // Props
+        this.card = card;
+        this.arrowBtn = card.querySelector('.card-title i');
+        this.cardText = card.querySelector('p');
+        this.cardImg = card.querySelector('img');
 
+        this.arrowBtn.addEventListener('click', () => this.toggleContent());
     }
     // Methods
+    toggleContent() {
+        console.log(`Clicked on ${this}!`);
+        this.arrowBtn.classList.toggle('rotate');
+
+        if (this.cardText.classList.contains('block')){
+            TweenMax.to(this.card, .3, {height: '63px'});
+            setTimeout(() => {
+                this.cardText.classList.toggle('block');
+                this.cardImg.classList.toggle('block');     
+            }, 300);
+        } else {
+            TweenMax.to(this.card, .3, {height: '625px'});
+            this.cardText.classList.toggle('block');
+            this.cardImg.classList.toggle('block');
+        }
+    }
 }
 
 const cards = document.querySelectorAll(".card");
-cards.forEach(card => new card(card));
+cards.forEach(card => new Card(card));
